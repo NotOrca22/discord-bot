67ffc46d87302a9c42ce18b9d13820f789e55c9b
git <547066921@qq.com>
2020-08-23 20:33:51 -0400

    Added lesson 4/5

--- lesson_04/lesson_4.py
+++ lesson_04/lesson_4.py
@@ -2,6 +2,7 @@ import discord
 import os
 from discord.ext import commands
 from discord.ext.commands import has_permissions
+from random import randint, random
 
 TOKEN = os.getenv("TOKEN")
 client = commands.Bot(command_prefix='!', case_insensitive=True)
@@ -28,8 +29,35 @@ async def kick(ctx, member: discord.Member, *, reason=None):
 @client.command()
 @has_permissions(ban_members=True)
 async def ban(ctx, member: discord.Member, *, reason=None):
-    await member.ban(reason=reason)
-    await ctx.channel.send(f"{member} has been banned by {ctx.message.author}. Reason: {reason}.")
+    a = ctx.guild.roles
+    reason = 'None specified'
+    a.reverse()
+    for r in a:
+        if r not in member.roles:
+            if r in ctx.message.author.roles:
+                channel = await member.create_dm()
+                await channel.send(
+                    f'{member.name}, you were banned from the server by {ctx.message.author}.'
+                    f' | Reason: {reason}. You may contact them to appeal or get reinvited.'
+                )
+                await member.ban(reason=reason)
+                print(ctx.message.content)
+                await ctx.channel.send('%s has been banned by %s. Reason: %s' % (member, ctx.message.author, reason))
+
+
+@client.command()
+async def coin(ctx, call):
+    if call == 'heads' or call == 'Heads' or call == 'HEADS':
+        if random() <= 0.5:
+            await ctx.channel.send('%s, you win!  The coin came up heads!' % ctx.message.author)
+        else:
+            await ctx.channel.send('%s, you lost!  The coin came up tails! Sorry! (not really)' % ctx.message.author)
+    if call == 'tails' or call == 'Tails' or call == 'TAILS':
+        a = random()
+        if a >= 0.5:
+            await ctx.channel.send('%s, you win!  The coin came up tails!' % ctx.message.author)
+        else:
+            await ctx.channel.send('%s, you lost!  The coin came up heads! Sorry! (not really)' % ctx.message.author)
 
 
 client.run(TOKEN)
